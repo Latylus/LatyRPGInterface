b0338bb1119b5ee2cc3253e8663a9be552b84168
Latylus <pierre.lataillade@gmail.com>
2021-03-20 19:26:35 +0100

    initializing Heroku connection

--- laty-RPG-app/client/src/api/index.js
+++ laty-RPG-app/client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://laty-rpg-interface.herokuapp.com/api',
 })
 
 export const insertCharacter = payload => api.post(`/character`, payload)
